e66d8e233fb50c02b4d54860239760baf4fa5a5e
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-15 21:10:36 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

---
+++ src/main/java/first/MVC/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,48 @@
+package first.MVC.web.servlet;
+
+import first.MVC.domain.member.Member;
+import first.MVC.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username"); // getParameter 값 꺼낸다.
+        int age = Integer.parseInt(request.getParameter("age")); // 나이는 문자라 변환 필요하다
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        // 결과를 html 로 받을것이다. (정적이 아닌 동적 html 로)
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter w = response.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+member.getId()+"</li>\n" +
+                " <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
