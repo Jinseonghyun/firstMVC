7ea152f7b1762446bdf558a55ad2f31724395771
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-16 00:36:06 +0900

    프론트 컨트롤러 도입

---
+++ src/main/java/first/MVC/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,18 @@
+package first.MVC.web.frontcontroller.v1.controller;
+
+import first.MVC.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);  // 외부에서 new-form.jsp이 불려지기를 원하지 않아서 항상 컨트롤러를 거치게하기 위함
+        dispatcher.forward(request,response); // 다른 서블릿이나 JSP로 이동할 수 있는 기능, 서버 내부에서 다시 호출이 발생
+    }
+}
