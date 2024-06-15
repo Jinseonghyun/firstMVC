f4b898b92bb44c5d2b6260032a147d7ddeb20468
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-16 00:56:48 +0900

    View 분리 - v2

---
+++ src/main/java/first/MVC/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,19 @@
+package first.MVC.web.frontcontroller.v2.controller;
+
+import first.MVC.web.frontcontroller.MyView;
+import first.MVC.web.frontcontroller.v2.ControllerV2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
---
+++ src/main/java/first/MVC/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,27 @@
+package first.MVC.web.frontcontroller.v2.controller;
+
+import first.MVC.domain.member.Member;
+import first.MVC.domain.member.MemberRepository;
+import first.MVC.web.frontcontroller.MyView;
+import first.MVC.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+        return new MyView("/WEB-INF/views/members.jsp");
+
+    }
+}
