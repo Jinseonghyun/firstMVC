e18017786e2555b92a5b35a4554cfdbf214ac53c
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-16 12:39:42 +0900

    스프링MVC - 시작하기

--- src/main/java/first/MVC/MvcApplication.java
+++ src/main/java/first/MVC/MvcApplication.java
@@ -1,8 +1,10 @@
 package first.MVC;
 
+import first.MVC.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +13,9 @@ public class MvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MvcApplication.class, args);
 	}
-
+//
+//	@Bean
+//	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+//		return new SpringMemberFormControllerV1();
+//	}
 }
